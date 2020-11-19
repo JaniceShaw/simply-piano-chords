bdd60b8ee04573d2ffdb610e94c26341d711efd4
Janice Shaw <hello@janiceshaw.co.uk>
2020-11-19 17:38:24 +0100

    changed way notes etc positioned

--- src/components/NoteDisplay/NoteDisplay.js
+++ src/components/NoteDisplay/NoteDisplay.js
@@ -3,6 +3,9 @@ import './NoteDisplay.scss';
 import { ReactComponent as TrebleClef } from './svg/treble-clef-e.svg';
 import { ReactComponent as BassClef } from './svg/bass-clef.svg';
 import { ReactComponent as Note } from './svg/note.svg';
+import { ReactComponent as Sharp } from './svg/sharp.svg';
+import { ReactComponent as Flat } from './svg/flat.svg';
+import { ReactComponent as Ledger } from './svg/ledger.svg';
 
 // function janTest(tina) {
 
@@ -40,7 +43,7 @@ class NoteDisplay extends React.Component {
                     <div className="NoteDisplay__sel-note">{selNote[0]}</div>
                     <TrebleClef />
                     <div className="chord">
-                        {selectedNotesRight.map((n, i) => <div key={i} className={'note note_' + n[1] + n[0]}><Note /></div>)
+                        {selectedNotesRight.map((n, i) => <div key={i} className={'note note_' + n[1] + n[0]}><Note />{n[1].endsWith("s") ? <i><Sharp /></i> : ""}{n[1].endsWith("f") ? <i><Flat /></i> : ""}{(n[1] === "C" && n[0] === 4) || (n[1] === "Cs" && n[0] === 4) ? <i><Ledger /></i> : ""}{(n[1] === "B" && n[0] === 3) || (n[1] === "Bf" && n[0] === 3) ? <i><Ledger /></i> : ""}</div>)
                         }
                     </div>
                 </div>
